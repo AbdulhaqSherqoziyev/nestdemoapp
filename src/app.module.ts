feb29a466074364eadd45066d2d92b6ef5776c86
AbdulhaqSherqoziyev <abdulhaqsherqoziyev@gmail.com>
2025-04-04 14:34:07 +0500

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,45 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { CommentsModule } from './comments/comments.module';
-import { User } from './users/entities/user.entity';
-import { Post } from './posts/entities/post.entity';
-import { Comment } from './comments/entities/comment.entity';
-import mongodbConfig from './config/mongodb.config';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [mongodbConfig],
     }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_NAME'),
-        entities: [User, Post, Comment],
-        synchronize: true,
-        autoLoadEntities: true,
-      }),
-    }),
-    MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        uri: configService.get<string>('mongodb.uri'),
-        user: configService.get<string>('mongodb.user'),
-        pass: configService.get<string>('mongodb.password'),
-      }),
-    }),
+    PrismaModule,
     UsersModule,
     PostsModule,
     CommentsModule,
